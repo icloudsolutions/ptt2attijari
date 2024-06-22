0aff7fb8709f9b71fafcb2b0900da7f6060299a9
Icloud Solutions <151461104+icloudsolutions@users.noreply.github.com>
2024-06-22 18:29:06 +0100

    Update service.py

--- src/service.py
+++ src/service.py
@@ -1,7 +1,8 @@
 import logging
 from datetime import datetime
 from dao import VirementDAO, HeaderDAO, BodyDAO, BodyLineDAO, BankDAO
-from models import Virement, Header, BodyLine, Bank
+from tnrib import TNRIB
+import os
 
 class VirementService:
 
@@ -33,29 +34,25 @@ class VirementService:
 
 class ValidationService:
     @staticmethod
-    def validate_virement(virement, bank):
+    def validate_virement(virement, bank,filename):
         try:
             ValidationService._validate_header(virement.header, bank)
             for body_line in virement.body:
                 ValidationService._validate_bodyline(body_line, bank)
-
             montant_total = int(virement.header.montant_total)
             montant_virements = sum(int(line.montant_virement) for line in virement.body)
-
             nbr_virement = int(virement.header.nbr_virement)
             count_virement = len(virement.body)
-
             valid_montant = montant_total == montant_virements
             valid_count = nbr_virement == count_virement
-                
-            print("\nValidation Test Results :")
-            print(f"Le Montant total du bordereau est égal à la somme des montants des virements soit {montant_virements/1000} dinars : Test {'Valid' if valid_montant else 'Invalid'}")
+            print(f"\nValidation Test Results of file {filename} :")
+            print(f"Le Montant total du bordereau {montant_total/1000} {' est égal ' if valid_montant else 'n est pas égal'} à la somme des montants des virements soit {montant_virements/1000} dinars : Test {'Valid' if valid_montant else 'Invalid'}")
             print(f"Le nombre total de virements est égal au nombre de lignes dans le corps du fichier soit {nbr_virement} virements : Test {'Valid' if valid_count else 'Invalid'}")
             logging.info(f"Validation montant total du bordereau soit {montant_virements/1000} dinars : {'Valid' if valid_montant else 'Invalid'}")
             logging.info(f"Validation nombre de virements {nbr_virement}: {'Valid' if valid_count else 'Invalid'}")
-            
+
         except AssertionError as e:
-            logging.error(f"Validation failed: {e}")
+            logging.error(f"Validation failed of {filename} : {e}")
             raise
 
     @staticmethod
@@ -73,6 +70,7 @@ class ValidationService:
             assert header.code_enregistrement == "11", f"Code enregistrement invalid: {header.code_enregistrement}. It must be 11"
             assert header.code_devise == "788", f"Code devise invalid: {header.code_devise}. It must be 788 for dinars"
             assert header.rang == "00", f"Rang invalid: {header.rang}. It must be '00'"
+
         except AssertionError as e:
             logging.error(f"Validation failed in Header: {e}")
             raise
@@ -96,71 +94,91 @@ class ValidationService:
             assert body_line.situation_donneur == "0", f"Situation du donneur d’ordres invalid: {body_line.situation_donneur}. It must be 1 (Résident)"
             assert body_line.type_compte_donneur == "1", f"Type du compte du donneur d’ordres invalid: {body_line.type_compte_donneur}. It must be 1 (Compte en dinars)"
             assert body_line.nature_compte_donneur == "0", f"Nature du compte du donneur d’ordres invalid: {body_line.nature_compte_donneur}. It must be '0' (pas d'exigence d’un dossier de change et de commerce extérieur)"
+            assert TNRIB(body_line.rib_emetteur).valid , f"Le RIB de l'émetteur invalid: {body_line.rib_emetteur}."
+            assert TNRIB(body_line.rib_beneficiaire).valid , f"Le RIB du bénéficiaire invalid: {body_line.rib_beneficiaire}."
+
         except AssertionError as e:
             logging.error(f"Validation failed in BodyLine: {e}")
             print(f"Validation failed in BodyLine ref: {body_line.ref_dossier}: {e}")
             raise
 
+class ProcessingService:
+            def processing_vir_batch(input_file,output_directory,target_bank_name):
+                # Log the start of execution
+                logging.info('Processing vir Batch started.')
+                filename_without_extension = os.path.splitext(os.path.basename(input_file))[0]
+                output_file = os.path.join(output_directory, f"{filename_without_extension}.vir")
+                # Get virement from file
+                virement_in = VirementService.get_virement(input_file)
+                # Determine bank_name from header.code_remettant
+                code_bank = virement_in.header.code_remettant
+                bank_in = BankService.get_bank_by_code(code_bank)
+                if not bank_in:
+                    raise ValueError(f"Bank code '{code_bank}' from header not found in bank data")
+                bank_out = BankService.get_bank_by_name(target_bank_name)
+                if not bank_out:
+                    raise ValueError(f"Bank code '{target_bank_name}' from config file not found in bank data")
+                logging.info(f"Bank name determined from header : {bank_in['name']}")
+                logging.info(f"Bank name determined from config file : {bank_out['name']}")
+                # Validate virement in relation to the determined bank
+                ValidationService.validate_virement(virement_in, bank_in,input_file)
+                virement_out = VirementService.convert_virement(virement_in,bank_in,bank_out)
+                # Save virement to an output file after validation
+                VirementService.save_virement(virement_out, output_file)
+                virement = VirementService.get_virement(output_file)
+                ValidationService.validate_virement(virement, bank_out,output_file)
+                logging.info(f"Virement processing completed successfully")
 
+            def processing_vir_file(input_file,target_bank_name,output_file):
+                # Log the start of execution
+                logging.info('Processing vir file started.')
+                # Get virement from file
+                virement_in = VirementService.get_virement(input_file)
+                # Determine bank_name from header.code_remettant
+                code_bank = virement_in.header.code_remettant
+                bank_in = BankService.get_bank_by_code(code_bank)
+                if not bank_in:
+                    raise ValueError(f"Bank code '{code_bank}' from header not found in bank data")
+                bank_out = BankService.get_bank_by_name(target_bank_name)
+                if not bank_out:
+                    raise ValueError(f"Bank code '{target_bank_name}' from config file not found in bank data")
+                logging.info(f"Bank name determined from header : {bank_in['name']}")
+                logging.info(f"Bank name determined from config file : {bank_out['name']}")
+                # Validate virement in relation to the determined bank
+                ValidationService.validate_virement(virement_in, bank_in,input_file)
+                virement_out = VirementService.convert_virement(virement_in,bank_in,bank_out)
+                # Save virement to an output file after validation
+                VirementService.save_virement(virement_out, output_file)
+                virement = VirementService.get_virement(output_file)
+                ValidationService.validate_virement(virement, bank_out,output_file)
+                logging.info(f"Virement processing completed successfully")
 
 class HeaderService:
 
     @staticmethod
     def get_header(file_path):
-        """
-        Get Header from file.
-        :param file_path: Path to the file to be read.
-        :return: Header object.
-        """
         return HeaderDAO.get_from_file(file_path)
 
 class BodyService:
 
     @staticmethod
     def get_body(file_path):
-        """
-        Get Body from file.
-        :param file_path: Path to the file to be read.
-        :return: List of BodyLine objects.
-        """
         return BodyDAO.get_from_file(file_path)
 
     @staticmethod
     def get_specific_body_lines(file_path, ids):
-        """
-        Get specific BodyLine objects from file by their indices.
-        :param file_path: Path to the file to be read.
-        :param ids: List of indices of the lines to be read.
-        :return: List of BodyLine objects.
-        """
         return BodyDAO.get_lines_from_file(file_path, ids)
 
     @staticmethod
     def get_body_line(file_path, id):
-        """
-        Get a specific BodyLine from file by its index.
-        :param file_path: Path to the file to be read.
-        :param id: Index of the line to be read.
-        :return: BodyLine object.
-        """
         return BodyLineDAO.get_from_file(file_path, id)
 
 class BankService:
 
     @staticmethod
     def get_bank_by_name(bank_name):
-        """
-        Get bank information by name.
-        :param bank_name: Name of the bank.
-        :return: Bank information dictionary or None if not found.
-        """
         return BankDAO.get_bank_by_name(bank_name)
 
     @staticmethod
     def get_bank_by_code(bank_code):
-        """
-        Get bank information by name.
-        :param bank_name: Name of the bank.
-        :return: Bank information dictionary or None if not found.
-        """
         return BankDAO.get_bank_by_code(bank_code)
